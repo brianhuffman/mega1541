c0d013895328dc03e25976eeee46943b007f72ad
Brian Huffman <huffman@galois.com>
2015-02-12 10:32:29 -0800

    Main loop outputs timing information over serial port

--- mega1541.ino
+++ mega1541.ino
@@ -8,11 +8,18 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(57600);
   mem_reset();
   cpu_reset();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  cpu_main(1000);
+  unsigned long time1 = micros();
+  cpu_main(1000000);
+  unsigned long time2 = micros();
+  unsigned long delta = time2 - time1;
+  Serial.println(delta);
+  delayMicros(1000000 - delta);
 }
+
